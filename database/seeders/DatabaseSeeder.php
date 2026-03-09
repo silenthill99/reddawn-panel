7e479f71a1e2cedc21e310997af41bd7f44f0945
silenthill99 <florian.graziani@sfr.fr>
2026-03-09 18:37:47 +0100

    Correction d'une faute de frappe

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([RoleSeeder::class, USerSeeder::class]);
+        $this->call([RoleSeeder::class, UserSeeder::class]);
     }
 }
